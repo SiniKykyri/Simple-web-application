32461cd6ebd6f022caa807bf1346a938eddb11f7
SiniKykyri <sini.kykyri@gmail.com>
2023-09-20 12:19:18 +0300

    Dark mode btn styling

--- assets/javascript/theme.js
+++ assets/javascript/theme.js
@@ -1,9 +1,22 @@
 
 const themeToggle = document.getElementById('themeBtn');
 
-function toggleDarkTheme(){
+function toggleDarkTheme() {
     document.body.classList.toggle('dark-theme');
+
+
+const icons = document.querySelectorAll('#themeBtn .material-icons');
+
+icons.forEach(icon => {
+    if(document.body.classList.contains('dark-theme')) {
+        icon.textContent = 'light_mode';
+    }
+    else {
+        icon.textContent = 'dark_mode';
+    }
+});
 }
+
 
 themeToggle.addEventListener('click', toggleDarkTheme);
 
