f0b55d329685f58ff335796f63a5045f0c109e5e
SiniKykyri <sini.kykyri@gmail.com>
2023-10-06 12:24:51 +0300

    Button added

---
+++ assets/javascript/btnScript.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const btn = document.getElementById("clickbtn");
+    submitButton.addEventListener("click", alertMessage);
+    
+ 
+    function alertMessage(){
+
+        alert("Painoit onnistuneesti nappia! :)");
+
+    }
+    });
