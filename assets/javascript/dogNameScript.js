605b3d7f909abf561cbc29d74a0f51fcf57f3e20
SiniKykyri <sini.kykyri@gmail.com>
2023-10-06 17:10:19 +0300

    logic testing

--- assets/javascript/dogNameScript.js
+++ assets/javascript/dogNameScript.js
@@ -1,39 +1,44 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-    const gender="";
-
+   
+    let gender;
     const btn = document.getElementById("submitName");
-    btn.addEventListener("click", submitClicked);
+    btn.addEventListener("click", saveName);
 
-    function submitClicked(){
-        alert("submit clicked");
-
-    }
-    const possibleAnswears = {
+    const possibleAnswers = {
         boyorgirl: "girl",
         boyorgirl: "boy",
         boyorgirl: "both",
     };
 
     function saveResults(){
-        const form = document.querySelector("nameForm");
-        const resultsContainer = document.getElementById("nimilista");
+        const form = document.querySelector("#nameForm");
+        const resultsContainer = document.getElementById("lista");
+        const nimilista = document.getElementById("nimilista");
 
-        for ( const question in possibleAnswears) {
+        for ( const question in possibleAnswers) {
             const selectedOption = form[question].value;
             if (selectedOption === possibleAnswers[question]){
-                gender=selectedOption;
+               gender=selectedOption;
+               console.log(gender);
+               
             }
         }
-        resultsContainer.innerHTML = gender;
     }
+        
+        function saveName(){
+            const nameInput = document.getElementById("name").value;
+
+            if(nameInput.trim()!==""){
     
+                const listItem = document.createElement("li");
+                listItem.textContent = nameInput;
+                nimilista.appendChild(listItem);
+    
+                
+            }
 
 
-
-
-
-
-
+        }
+    
 
 });
