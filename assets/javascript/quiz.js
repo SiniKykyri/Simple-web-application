12e28a4da2d2a962e1d4cd1b3c0d928f3609ee0c
SiniKykyri <sini.kykyri@gmail.com>
2023-10-17 18:14:07 +0300

    Quiz improved

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -12,6 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
     function createImage(src, parent) {
         const img = document.createElement("img");
         img.src = src;
+        img.id = "resultImage";
         parent.appendChild(img);
     }
 
@@ -22,41 +23,49 @@ document.addEventListener('DOMContentLoaded', function () {
 
         for (const question in correctAnswers) {
             const selectedOption = form[question].value;
-            if (selectedOption === correctAnswers[question]) {
+            const isCorrect = selectedOption === correctAnswers[question];
+
+            const icon = document.getElementById(`${question}-icon`);
+            icon.classList.remove("fa-times-circle,fa-check-circle");
+            icon.classList.add(isCorrect ? "fa-check-circle":"fa-times-circle");
+            icon.style.color = isCorrect ? "green" : "red";
+            if(isCorrect) {
                 score++;
             }
         }
 
+     
         
 
         if (score === 0) {
             resultsContainer.innerHTML += ` Pisteesi ovat: ${score} / ${Object.keys(correctAnswers).length}<br>`;
-            resultsContainer.innerHTML += "Höh! Et saanut ainuttakaan pistettä. Kokeilehan uudestaan!";
+            resultsContainer.innerHTML += "Höh! Et saanut ainuttakaan pistettä. Kokeilehan uudestaan!<br>";
             createImage("assets/pictures/upset.jpg", resultsContainer);
             return;
         }
         else if (score === 1) {
             resultsContainer.innerHTML += ` Pisteesi ovat: ${score} / ${Object.keys(correctAnswers).length}<br>`;
-            resultsContainer.innerHTML += "Sait yhden pisteen. Oliko tämä nyt tietoa vai arvausta?";
+            resultsContainer.innerHTML += "Sait yhden pisteen. Oliko tämä nyt tietoa vai arvausta?<br>";
             createImage("assets/pictures/akita.jpg", resultsContainer);
             return;
         }
         else if (score === 2) {
             resultsContainer.innerHTML += ` Pisteesi ovat: ${score} / ${Object.keys(correctAnswers).length}<br>`;
-            resultsContainer.innerHTML += "Sait kaksi pistettä. Jippii!";
+            resultsContainer.innerHTML += "Sait kaksi pistettä. Jippii!<br>";
             createImage("assets/pictures/happy.jpg", resultsContainer);
 
             return;
         }
         else if (score === 3) {
             resultsContainer.innerHTML += ` Pisteesi ovat: ${score} / ${Object.keys(correctAnswers).length}<br>`;
-            resultsContainer.innerHTML += "Sait kaikki kolme pistettä. Hienoa! Olet oikein koiratietäjä!";
+            resultsContainer.innerHTML += "Sait kaikki kolme pistettä. Hienoa! Olet oikein koiratietäjä!<br>";
             createImage("assets/pictures/winner.jpg", resultsContainer);
             return;
         }
+    
 
        
 
-
-    } 
+    }
+    
 });
