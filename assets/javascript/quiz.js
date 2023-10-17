da249342b541bc7f6a77d76a22e388871a489ea6
SiniKykyri <sini.kykyri@gmail.com>
2023-10-17 17:33:10 +0300

    Modified theme js

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -1,29 +1,62 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
-const submitButton = document.getElementById("Submit");
-submitButton.addEventListener("click", calculateResults);
+    const submitButton = document.getElementById("Submit");
+    submitButton.addEventListener("click", calculateResults);
 
+    const correctAnswers = {
+        luovutus: "8",
+        montako: "2",
+        kauanko: "2"
+    };
 
-
-const correctAnswers = {
-    luovutus: "8",
-    montako: "2",
-    kauanko: "2"
-};
-
-function calculateResults(){
-    const form = document.querySelector("form");
-    const resultsContainer = document.getElementById("quizResults");
-    let score = 0;
-
-    for ( const question in correctAnswers) {
-        const selectedOption = form[question].value;
-        if (selectedOption === correctAnswers[question]){
-            score++;
-        }
+    function createImage(src, parent) {
+        const img = document.createElement("img");
+        img.src = src;
+        parent.appendChild(img);
     }
-    resultsContainer.innerHTML =` Pisteesi ovat: ${score} / ${Object.keys(correctAnswers).length}`;
 
-   
-}
+    function calculateResults() {
+        const form = document.querySelector("form");
+        const resultsContainer = document.getElementById("quizResults");
+        let score = 0;
+
+        for (const question in correctAnswers) {
+            const selectedOption = form[question].value;
+            if (selectedOption === correctAnswers[question]) {
+                score++;
+            }
+        }
+
+        
+
+        if (score === 0) {
+            resultsContainer.innerHTML += ` Pisteesi ovat: ${score} / ${Object.keys(correctAnswers).length}<br>`;
+            resultsContainer.innerHTML += "Höh! Et saanut ainuttakaan pistettä. Kokeilehan uudestaan!";
+            createImage("assets/pictures/upset.jpg", resultsContainer);
+            return;
+        }
+        else if (score === 1) {
+            resultsContainer.innerHTML += ` Pisteesi ovat: ${score} / ${Object.keys(correctAnswers).length}<br>`;
+            resultsContainer.innerHTML += "Sait yhden pisteen. Oliko tämä nyt tietoa vai arvausta?";
+            createImage("assets/pictures/akita.jpg", resultsContainer);
+            return;
+        }
+        else if (score === 2) {
+            resultsContainer.innerHTML += ` Pisteesi ovat: ${score} / ${Object.keys(correctAnswers).length}<br>`;
+            resultsContainer.innerHTML += "Sait kaksi pistettä. Jippii!";
+            createImage("assets/pictures/happy.jpg", resultsContainer);
+
+            return;
+        }
+        else if (score === 3) {
+            resultsContainer.innerHTML += ` Pisteesi ovat: ${score} / ${Object.keys(correctAnswers).length}<br>`;
+            resultsContainer.innerHTML += "Sait kaikki kolme pistettä. Hienoa! Olet oikein koiratietäjä!";
+            createImage("assets/pictures/winner.jpg", resultsContainer);
+            return;
+        }
+
+       
+
+
+    } 
 });
--- assets/javascript/theme.js
+++ assets/javascript/theme.js
@@ -10,13 +10,25 @@ const icons = document.querySelectorAll('#themeBtn .material-icons');
 icons.forEach(icon => {
     if(document.body.classList.contains('dark-theme')) {
         icon.textContent = 'light_mode';
+        document.cookie = "theme=dark";  
     }
     else {
         icon.textContent = 'dark_mode';
+        document.cookie = "theme=light";
     }
 });
 }
 
+function checkThemePreference(){
+    const themeCookie = document.cookie.split(';').find(cookie => cookie.startsWith('theme='));
+    if(themeCookie) {
+        const themeValue = themeCookie.split('=')[1];
+        if(themeValue === 'dark') {
+            document.body.classList.add('dark-theme');
+        }
+    }
+}
+document.addEventListener('DOMContentLoaded', checkThemePreference);
 
 themeToggle.addEventListener('click', toggleDarkTheme);
 
